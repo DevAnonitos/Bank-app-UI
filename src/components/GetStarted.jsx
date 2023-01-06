6645ec559354e7678575d7db5bc830ac79ca589f
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-06 21:04:53 +0700

    update-Hero-section@2

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -10,7 +10,7 @@ const GetStarted = () => {
           ${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`
         }
       >
-        <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+        <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full hover:brightness-200`}>
 
         </div>
       </div>
