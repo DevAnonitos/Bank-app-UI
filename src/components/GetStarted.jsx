36b7d02d7528d413b305ea2f9805aeab5857ef16
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-06 16:38:54 +0700

    update-GetStarted

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import styles from '../style';
+import { arrowUp } from '../assets';
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
+    <>
+      <div 
+        className={`
+          ${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`
+        }
+      >
+        <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+
+        </div>
+      </div>
+    </>
   )
 }
 
