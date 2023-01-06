3a7e618708f25a426b66a6d1331a0804b8f48f7b
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-06 10:05:57 +0700

    update-Structure-layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
 import React from 'react'; 
+import styles from './style';
+
+import { 
+  Billing, 
+  Business, 
+  CardDeal, 
+  Clients, 
+  CTA, 
+  Footer, 
+  Navbar, 
+  Stats, 
+  Testimonials, 
+  Hero 
+} from "./components";
 
 function App() {
  
   return (
-    <div className="App">
-      
+    <div className="bg-primary w-full overflow-hidden">
+      {/*================== Navbar========================= */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+        {/* ================Hero section ================== */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      {/*=================== Components==================== */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+
     </div>
   )
 }
