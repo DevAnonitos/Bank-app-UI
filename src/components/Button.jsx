e1e7d7c085997844024667c39e0ec968fe2cf3b0
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-07 20:26:00 +0700

    update-Business@2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>Button</div>
+    <>
+      <button 
+        type='button' 
+        className={`
+          py-4 px-6 bg-blue-gradient font-poppins 
+          font-medium text-[18px] text-primary 
+          outline-none shadow-emerald-500 border-none rounded-full ${styles}`
+        }
+      >
+        Get started
+      </button>
+    </>
   )
 }
 
