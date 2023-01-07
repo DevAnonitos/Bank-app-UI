e63439e394b9c540be91a6e965aee673b02ab52a
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-07 22:29:51 +0700

    update-billing

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 
 const Billing = () => {
   return (
-    <div>Billing</div>
+    <>
+      <section id='product' className={layout.sectionReverse}>
+        <div className={layout.sectionImgReverse}>
+          <img 
+            src={bill} 
+            alt="billing" 
+            className='w-[100%] h-[100%] relative z-[5]'
+          />
+          <div 
+            className='absolute z-[3] -left-1/2 
+            top-0 w-[50%] h-[50%] rounded-full white__gradient' 
+          />
+          <div 
+            className='absolute z-[0] -left-1/2 
+            bottom-0 w-[50%] h-[50%] rounded-full pink__gradient'  
+          />
+        </div>  
+
+        <div className={layout.sectionInfo}>
+
+        </div>   
+      </section>
+    </>
   )
 }
 
