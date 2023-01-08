263c930ed150f60a5df5366a16270da0bc721743
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-08 21:27:03 +0700

    update-Billing@2

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -23,7 +23,32 @@ const Billing = () => {
         </div>  
 
         <div className={layout.sectionInfo}>
+          <h2 className={styles.heading2}>
+            Easily control your <br className="sm:block hidden" /> 
+            billing & invoicing
+          </h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+            Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+            aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+            placerat.
+          </p>
 
+          <div 
+            className="flex flex-row flex-wrap sm:mt-10 mt-6"
+          >
+            <img 
+              src={apple} 
+              alt="google_play" 
+              className="w-[128.86px] h-[42.05px] 
+              object-contain mr-5 cursor-pointer"     
+            />
+            <img 
+              src={google} 
+              alt="google_play" 
+              className="w-[144.17px] h-[43.08px] 
+              object-contain cursor-pointer" 
+            />
+          </div>
         </div>   
       </section>
     </>
